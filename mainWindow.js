a0955e52d1971a101edccaadb59421e17592635d
Samadzada,Polad P.S <p.samadzade@student.fontys.nl>
2020-03-20 00:20:24 +0100

    nav select

--- mainWindow.js
+++ mainWindow.js
@@ -1,7 +1,11 @@
-document.querySelectorAll(".item").onclick = () => {
-  console.log("S");
-};
+$('a.item').on('click', handleNavClick);
 
 window.navigationBar = {
   navBarContainer: document.getElementById("navbar")
 };
+
+function handleNavClick(e)
+{
+  $('a.item.active').removeClass('active');
+  $(this).addClass('active');
+}
