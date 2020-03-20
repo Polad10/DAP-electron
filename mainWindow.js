e6c275d97b7376718e04e7a610f883bcfe1fcfb6
Gunawan <amelia.gunawan@accenture.com>
2020-03-20 01:31:13 +0100

    added partial reload for different pages

--- mainWindow.js
+++ mainWindow.js
@@ -1,11 +1,21 @@
-$('a.item').on('click', handleNavClick);
+let firstLoad = true;
 
-window.navigationBar = {
-  navBarContainer: document.getElementById("navbar")
-};
+$("a.item").on("click", handleNavClick);
 
-function handleNavClick(e)
-{
-  $('a.item.active').removeClass('active');
-  $(this).addClass('active');
+function handleNavClick(e) {
+  // prevent from going to the page
+  e.preventDefault();
+
+  //set Active class
+  $("a.item.active").removeClass("active");
+  $(this).addClass("active");
+
+  // get the href
+  var href = $(this).attr("href");
+  if (firstLoad) {
+    console.log("yes");
+    // $("#content").empty();
+    firstLoad = false;
+  }
+  $("#content").load(href, function() {});
 }
