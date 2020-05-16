d75b43f1afb5994aa5824256e2add441e226652e
Samadzada,Polad P.S <p.samadzade@student.fontys.nl>
2020-05-17 00:35:30 +0200

    new patient form

--- js/patientWindow.js
+++ js/patientWindow.js
@@ -1,1 +1,11 @@
 $("#patient_dropdown").dropdown({});
+
+$('#new_patient_btn').on('click', handleNewPatientBtnClick);
+
+function handleNewPatientBtnClick()
+{
+    $('#modal_content').load('./modals/new_patient.html', () => {
+        $('#patient_modal').modal('show');
+        $('#dob_calendar').calendar();
+    });
+}
