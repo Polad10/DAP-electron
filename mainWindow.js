7dbe239c41daf47b9101ca91a001ae404ced03fd
Samadzada,Polad P.S <p.samadzade@student.fontys.nl>
2020-05-29 19:35:58 +0200

    conflict resolved

--- mainWindow.js
+++ mainWindow.js
@@ -1,11 +1,6 @@
 //load homeWindow when loaded
-<<<<<<< HEAD
 $(document).ready(function() {
   $("#content").load("./pages/homeWindow.html", function() {});
-=======
-$(document).ready(function () {
-  $("#content").load("./pages/homeWindow.html", function () {});
->>>>>>> 88328b77e1602080f1c961c6b3f4efd114645d91
 });
 
 $("a.item").on("click", handleNavClick);
