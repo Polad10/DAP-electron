402d98b9b03d09927c6eed212267c390997a614e
ameliakezia <amelia.kezia.gunawan@gmail.com>
2020-08-20 22:53:30 +0200

    Added new treatment form in add new visit

--- js/new_va.js
+++ js/new_va.js
@@ -3,3 +3,9 @@ $("#new_patient_btn").on("click", handleAddPatientBtnClick);
 function handleAddPatientBtnClick() {
   $("#add_patient_content").transition("drop", "800ms");
 }
+
+$("#new_treatment_btn").on("click", handleAddTreatmentBtnClick);
+
+function handleAddTreatmentBtnClick() {
+  $("#add_treatment_content").transition("drop", "800ms");
+}
