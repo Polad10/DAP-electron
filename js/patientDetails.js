858b40fe76bc8f0fcc1f76d0703be60c02c0dfb5
ameliakezia <amelia.kezia.gunawan@gmail.com>
2020-09-06 22:00:36 +0200

    added animation on patient details and add/edit button

--- js/patientDetails.js
+++ js/patientDetails.js
@@ -13,3 +13,30 @@ function handlePatientMenuClick(e) {
   // get the href and load it
   $(`#${id}`).css({ display: "block" });
 }
+
+$("#treatment_list").on("click", handleTreatmentListItemClick);
+
+function handleTreatmentListItemClick() {
+  $("#treatment_details_placeholder").show();
+  $("#treatment_details_content").hide();
+  $(".treatment_details_payment_placeholder").show();
+  $(".treatment_details_payment_content").hide();
+  $(".treatment_details_loader").addClass("active");
+
+  setTimeout(function () {
+    $("#treatment_details_placeholder").hide();
+    $("#treatment_details_content").show();
+    $(".treatment_details_payment_placeholder").hide();
+    $(".treatment_details_payment_content").show();
+    $(".treatment_details_loader").removeClass("active");
+  }, 1500);
+}
+
+function initialize() {
+  // Fill in the treatment name list
+  setTimeout(function () {
+    $(".dimmer").removeClass("active");
+  }, 1500);
+}
+
+initialize();
